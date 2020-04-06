ef661d65c917fc7bb88683be9ee12743029e5b57
Brian Rease <36502488+brease568@users.noreply.github.com>
2020-04-06 19:18:57 -0400

    Update SQL/vetclinicData.sql
    - Changed pet name.
    
    Co-Authored-By: Bekah <rebekah.treadwell@gmail.com>

--- SQL/vetclinicData.sql
+++ SQL/vetclinicData.sql
@@ -1,6 +1,7 @@
 BEGIN TRANSACTION;
 INSERT INTO "clients" ("client_id","first_name","last_name","phone","email") VALUES (1,'john','doe','555-321-6543','johndoe@mail.com');
-INSERT INTO "pets" ("pet_id","name","species","gender","dob","owner") VALUES (1,'Isis','cat','FS','2006-11-01',1);
+INSERT INTO "pets" ("pet_id","name","species","gender","dob","owner") VALUES (1,'Lily','cat','FS','2006-11-01',1);
+
 INSERT INTO "appointments" ("date","time","client","pet","reason") VALUES ('2020-05-03','2020-05-03 11:00:00',1,1,'Annual Exam');
 INSERT INTO "users" ("user_id","username","password") VALUES (1,'bob','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8');
 COMMIT;
