8d629d02866bdf3aaa31cce93accac33e363047c
Brian <b.rease568@gmail.com>
2020-04-16 02:40:41 -0700

    - Changelog: -- Connected pieces of the front-end to the back-end. --- Users can successfully log in, view all clients, and create new clients. -- Back-end code for editing and deleting clients is added, but has not been attached to the front-end due to missing buttons. -- Added compiled jar file to run the program.

--- src/vetportal/Database.java
+++ src/vetportal/Database.java
@@ -8,10 +8,15 @@
 
 package vetportal;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.sqlite.SQLiteConfig;
+import org.sqlite.SQLiteErrorCode;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Database {
+
+    private String errorMessage;
 
     public static final String DB_NAME = "vetclinic.db";
     public static final String CONNECTION_STRING = "jdbc:sqlite:" + DB_NAME;
@@ -44,13 +49,14 @@ public class Database {
     public static final String COLUMN_APPOINTMENT_REASON = "reason";
 
     private Connection conn;
+    private Statement statement;
 
     //This method attempts to open a connection with the database
     public boolean open() {
         try {
             SQLiteConfig config = new SQLiteConfig();
-	    config.enforceForeignKeys(true);
-	    conn = DriverManager.getConnection(CONNECTION_STRING, config.toProperties());
+            config.enforceForeignKeys(true);
+            conn = DriverManager.getConnection(CONNECTION_STRING, config.toProperties());
             return true;
         } catch (SQLException eS) {
             System.out.println("SQLException: " + eS.getMessage());
@@ -69,5 +75,124 @@ public class Database {
         }
     } //end of close()
 
+    private void setErrorMessage(String message) {
+        errorMessage = message;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public int getClientID(String phoneNumber) {
+        try {
+            statement = conn.createStatement();
+            ResultSet idResult = statement.executeQuery("SELECT " + COLUMN_CLIENT_ID +
+                                                            " FROM " + TABLE_CLIENTS + " WHERE " +
+                                                            COLUMN_CLIENT_PHONE_NUMBER + "=\'" +
+                                                            phoneNumber + "\'");
+            return idResult.getInt("client_id");
+        } catch (SQLException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+            return -1;
+        }
+    } //end of getClientID()
+
+    /*
+    This method uses the Apache commons codec to hash a given 'password' String with sha256.
+    The hash is then compared to a password hash in the sqlite database
+     */
+    public boolean authenticate(String username, String password) {
+        try {
+            String passwordHash = DigestUtils.sha256Hex(password);
+
+            statement = conn.createStatement();
+            ResultSet authResult = statement.executeQuery("SELECT " + COLUMN_USER_PASSWORD +
+                                                                " FROM " + TABLE_USERS + " WHERE "
+                                                            + COLUMN_USER_NAME + "='" + username + "'");
+
+            if (passwordHash.equals(authResult.getString(COLUMN_USER_PASSWORD))) {
+                return true;
+            }
+            statement.close();
+            return false;
+        } catch (SQLException e) {
+            System.out.println("Authentication unsuccessful: " + e.getMessage());
+            return false;
+        }
+    } //end of authenticate()
+
+    public boolean insertClient(String firstName, String lastName, String phoneNumber, String email) {
+        try {
+            statement = conn.createStatement();
+            statement.execute("INSERT INTO " + TABLE_CLIENTS +
+                    " (" + COLUMN_CLIENT_FIRST_NAME +
+                    ", " + COLUMN_CLIENT_LAST_NAME +
+                    ", " + COLUMN_CLIENT_PHONE_NUMBER +
+                    ", " + COLUMN_CLIENT_EMAIL + ") VALUES (\'" +
+                    firstName + "\', \'" + lastName + "\', \'" + phoneNumber + "\', \'" + email + "\')");
+            statement.close();
+            return true;
+        } catch (SQLException e) {
+            if (SQLiteErrorCode.SQLITE_CONSTRAINT_UNIQUE.code == 2067) {
+                //System.out.println("The phone number you entered is already in the database! Please try again.");
+                setErrorMessage("The phone number you entered is already in the database! Please try again.");
+            } else {
+                //System.out.println("Unable to create new client.\n" + e.getMessage());
+                setErrorMessage("Unable to create new client.\n" + e.getMessage());
+            }
+            return false;
+        }
+    } //end of insertClient()
+
+    public boolean deleteClient(String phoneNumber) {
+        try {
+            statement = conn.createStatement();
+            statement.execute("DELETE FROM " + TABLE_CLIENTS +
+                    " WHERE " + COLUMN_CLIENT_PHONE_NUMBER +
+                    "=\'" + phoneNumber + "\'");
+            statement.close();
+            return true;
+        } catch (SQLException e) {
+            setErrorMessage("Unable to delete client.\n" + e.getMessage());
+            return false;
+        }
+    } //end of deleteClient()
+
+    public ArrayList<Clients> selectAllClients() {
+        try {
+            statement = conn.createStatement();
+            ResultSet results = statement.executeQuery("SELECT * FROM " + TABLE_CLIENTS);
+            ArrayList<Clients> allClients = new ArrayList<>();
+            while(results.next()) {
+                Clients client = new Clients(results.getInt(COLUMN_CLIENT_ID), results.getString(COLUMN_CLIENT_FIRST_NAME),
+                                            results.getString(COLUMN_CLIENT_LAST_NAME), results.getString(COLUMN_CLIENT_PHONE_NUMBER),
+                                            results.getString(COLUMN_CLIENT_EMAIL));
+                allClients.add(client);
+            }
+            results.close();
+            statement.close();
+            return allClients;
+        } catch (SQLException e) {
+            setErrorMessage("Could not find any clients.");
+            return null;
+        }
+    } //end of selectAllClients()
+
+    public boolean updateClient(int clientID, String firstName, String lastName, String phoneNumber, String email) {
+        try {
+            statement = conn.createStatement();
+            statement.execute("UPDATE " + TABLE_CLIENTS +
+                    " SET " + COLUMN_CLIENT_FIRST_NAME + "=\'" + firstName +
+                    "\', " + COLUMN_CLIENT_LAST_NAME + "=\'" + lastName +
+                    "\', " + COLUMN_CLIENT_PHONE_NUMBER + "=\'" + phoneNumber +
+                    "\', " + COLUMN_CLIENT_EMAIL + "=\'" + email +
+                    "\' WHERE " + COLUMN_CLIENT_ID + "=" + clientID);
+            statement.close();
+            return true;
+        } catch (SQLException e) {
+            setErrorMessage("Unable to update client.\n" + e.getMessage());
+            return false;
+        }
+    } //end of updateClient()
 
 } //end of Database
--- src/vetportal/VetPortal.java
+++ src/vetportal/VetPortal.java
@@ -12,30 +12,344 @@
 package vetportal;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class VetPortal extends JFrame {
     private static final long serialVersionUID = 123L;
 
+    private Database vetDatabase;
+
+    private String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                                "[a-zA-Z0-9_+&*-]+)*@" +
+                                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                                "A-Z]{2,7}$";
+    private Pattern emailPattern = Pattern.compile(emailRegex);
+
     //TODO: create all the objects for the main GUI:
+    private static VetPortal vetPortal;
+    private javax.swing.JButton loginBtn;
+    private javax.swing.JPanel loginPanel;
+    private javax.swing.JPasswordField passwordField;
+    private javax.swing.JLabel passwordLabel;
+    private javax.swing.JTextField usernameField;
+    private javax.swing.JLabel usernameLabel;
+    private javax.swing.JLabel welcomeLabel;
+    private javax.swing.JPanel welcomePanel;
+
+    //Create object for DashboardGui:
+    DashboardsGui dashboard;
 
     //constructor:
     public VetPortal() {
-        super("Vet Clinic");
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new BorderLayout(10, 10));
-        setSize(1600, 700);
+        super("Vet Portal");
 
+        loginPanel = new javax.swing.JPanel();
+        welcomePanel = new javax.swing.JPanel();
+        welcomeLabel = new javax.swing.JLabel();
+        usernameLabel = new javax.swing.JLabel();
+        passwordLabel = new javax.swing.JLabel();
+        usernameField = new javax.swing.JTextField();
+        loginBtn = new javax.swing.JButton();
+        passwordField = new javax.swing.JPasswordField();
+
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        loginPanel.setBackground(new java.awt.Color(122, 188, 255));
+        loginPanel.setToolTipText("");
+        loginPanel.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+
+        welcomePanel.setBackground(new java.awt.Color(255, 255, 255));
+
+        welcomeLabel.setBackground(new java.awt.Color(122, 188, 255));
+        welcomeLabel.setFont(new java.awt.Font("Calibri", 1, 24)); // NOI18N
+        welcomeLabel.setForeground(new java.awt.Color(122, 188, 255));
+        welcomeLabel.setText("Welcome to the Vet Portal");
+
+        usernameLabel.setFont(new java.awt.Font("Calibri", 1, 15)); // NOI18N
+        usernameLabel.setForeground(new java.awt.Color(122, 188, 255));
+        usernameLabel.setText("Username");
+
+        passwordLabel.setFont(new java.awt.Font("Calibri", 1, 15)); // NOI18N
+        passwordLabel.setForeground(new java.awt.Color(122, 188, 255));
+        passwordLabel.setText("Password");
+
+        usernameField.setFont(new java.awt.Font("Calibri", 1, 15)); // NOI18N
+        usernameField.setForeground(new java.awt.Color(100, 100, 100));
+        usernameField.setToolTipText("");
+
+        loginBtn.setBackground(new java.awt.Color(122, 188, 255));
+        loginBtn.setFont(new java.awt.Font("Calibri", 1, 15)); // NOI18N
+        loginBtn.setForeground(new java.awt.Color(255, 255, 255));
+        loginBtn.setText("Login");
+
+        passwordField.setFont(new java.awt.Font("Calibri", 1, 15)); // NOI18N
+
+        javax.swing.GroupLayout welcomePanelLayout = new javax.swing.GroupLayout(welcomePanel);
+        welcomePanel.setLayout(welcomePanelLayout);
+        welcomePanelLayout.setHorizontalGroup(
+                welcomePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(welcomePanelLayout.createSequentialGroup()
+                                .addGap(48, 48, 48)
+                                .addGroup(welcomePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(passwordLabel)
+                                        .addComponent(usernameLabel)
+                                        .addComponent(usernameField)
+                                        .addComponent(passwordField)
+                                        .addComponent(loginBtn, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 354, Short.MAX_VALUE))
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, welcomePanelLayout.createSequentialGroup()
+                                .addContainerGap(103, Short.MAX_VALUE)
+                                .addComponent(welcomeLabel)
+                                .addGap(99, 99, 99))
+        );
+        welcomePanelLayout.setVerticalGroup(
+                welcomePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(welcomePanelLayout.createSequentialGroup()
+                                .addGap(41, 41, 41)
+                                .addComponent(welcomeLabel)
+                                .addGap(36, 36, 36)
+                                .addComponent(usernameLabel)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(usernameField, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(36, 36, 36)
+                                .addComponent(passwordLabel)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(passwordField, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(38, 38, 38)
+                                .addComponent(loginBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(140, Short.MAX_VALUE))
+        );
+        javax.swing.GroupLayout loginPanelLayout = new javax.swing.GroupLayout(loginPanel);
+        loginPanel.setLayout(loginPanelLayout);
+        loginPanelLayout.setHorizontalGroup(
+                loginPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, loginPanelLayout.createSequentialGroup()
+                                .addGap(0, 284, Short.MAX_VALUE)
+                                .addComponent(welcomePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        loginPanelLayout.setVerticalGroup(
+                loginPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(welcomePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(loginPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(loginPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+
+        loginBtn.addActionListener(event -> authenticateUser());
         //TODO: create follow on Swing components and add them to the frame:
 
     } //end of constructor
 
     public static void main(String[] args) {
-        VetPortal window = new VetPortal();
-        window.setVisible(true);
+        vetPortal = new VetPortal();
+        vetPortal.setVisible(true);
+
+//        AddClient page = new AddClient();
+//        page.setVisible(true);
+
+        //vetPortal.authenticateUser();
+        //vetPortal.createClient();
+        //vetPortal.deleteClient();
+        //vetPortal.viewAllClients();
+        //vetPortal.editClient();
     } //end of main()
 
+    private boolean isAlphabetic(String stringToCheck) {
+        return stringToCheck.chars().allMatch(Character::isAlphabetic);
+    }
+
+    private boolean isValidEmail(String email) {
+        return emailPattern.matcher(email).matches();
+    }
+
+    public DashboardsGui getDashboard() {
+        return dashboard;
+    }
+
+    public Database getVetDatabase() {
+        return vetDatabase;
+    }
+
+    public VetPortal getVetPortal() {
+        return vetPortal;
+    }
+
+    /*
+    This method is used to authenticate a user by pulling a username and password
+    from a Java Swing GUI; specifically, a JTextfield and JPasswordfield
+     */
+    private void authenticateUser() {
+        String username = usernameField.getText();
+        String password = new String(passwordField.getPassword());
+
+        if ((username.isEmpty()) || (password.isEmpty())) { //Verify the username or password are not empty
+            System.out.println("Username or password can not be empty!");
+            return;
+        }
+
+        Database myDatabase = new Database();
+        if (!myDatabase.open()) { //Attempt to open a connection with the database
+            System.out.println("Can't connect to the database!");
+            return;
+        }
+
+        if (!myDatabase.authenticate(username, password)) { //Attempt actual authentication
+            //TODO: change implementation; make error display on GUI
+            System.out.println("Invalid username or password!");
+        } else {
+            System.out.println("Authentication successful!");
+            dashboard = new DashboardsGui(vetPortal);
+            vetPortal.setVisible(false);
+            vetPortal.viewAllClients();
+            dashboard.setVisible(true);
+        }
+        myDatabase.close();
+    } //end of authenticateUser()
+
     //TODO: add follow on methods
+    public void createClient(String firstName, String lastName, String phoneNumber, String email) {
+//        String firstName = "Elton";
+//        String lastName = "John";
+//        String phoneNumber = "543-123-5698";
+//        String email = "elton.john@yahoo.com";
+
+
+
+        if ((firstName.isEmpty()) || (lastName.isEmpty()
+                || (phoneNumber.isEmpty()) || (email.isEmpty()))) { //Verify if any fields are empty
+            System.out.println("All fields are required!");
+            return;
+        }
+
+        if (!(isAlphabetic(firstName) && isAlphabetic(lastName))) { //Verify the first and last name are alphabetic
+            String nameErrorMessage = "First and last name must be alphabetic!";
+            JOptionPane.showMessageDialog(null, nameErrorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        if (!isValidEmail(email)) { //Verify valid email address
+            String emailErrorMessage = "Invalid email address!";
+            JOptionPane.showMessageDialog(null, emailErrorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        //TODO: Possibly add error checking to see if phoneNumber is valid??
+
+
+        vetDatabase = new Database();
+        if (!vetDatabase.open()) { //Attempt to open a connection with the database
+            System.out.println("Can't connect to the database!");
+            return;
+        }
+
+        if (!vetDatabase.insertClient(firstName, lastName, phoneNumber, email)) { //Attempt actual INSERT
+            String errorMessage = vetDatabase.getErrorMessage();
+            JOptionPane.showMessageDialog(null, errorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+        } else {
+            //TODO: change implementation to refresh the view of all clients
+            System.out.println("Creating client was successful!");
+        }
+        vetDatabase.close();
+    } //end of createClient()
+
+    private void deleteClient() {
+        String phoneNumber = "543-123-5691";
+
+        vetDatabase = new Database();
+        if (!vetDatabase.open()) { //Attempt to open a connection with the database
+            System.out.println("Can't connect to the database!");
+            return;
+        }
+
+        if (!vetDatabase.deleteClient(phoneNumber)) {
+            String errorMessage = vetDatabase.getErrorMessage();
+            JOptionPane.showMessageDialog(null, errorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+        } else {
+            System.out.println("Deleting client was successful!");
+        }
+        vetDatabase.close();
+    } //end of deleteClient()
+
+    public void viewAllClients() {
+        vetDatabase = new Database();
+        if (!vetDatabase.open()) { //Attempt to open a connection with the database
+            System.out.println("Can't connect to the database!");
+            return;
+        }
+
+        ArrayList<Clients> allClients = vetDatabase.selectAllClients();
+
+        if (allClients.isEmpty()) {
+            String errorMessage = vetDatabase.getErrorMessage();
+            JOptionPane.showMessageDialog(null, errorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+        } else {
+            for (Clients client: allClients) {
+                System.out.println("client_id: " + client.getClientID() + ", First Name: " + client.getClientFirstName());
+            }
+            DefaultTableModel model = (DefaultTableModel)dashboard.getClientsTable().getModel();
+            for (Clients client : allClients) {
+                Object[] row = {client.getClientFirstName(), client.getClientLastName(), client.getClientEmail(), client.getClientPhoneNumber()};
+                model.addRow(row);
+            }
+        }
+        vetDatabase.close();
+    } //end of viewAllClients()
+
+    private void editClient() {
+        String currentPhoneNumber = "111-111-1111";
+
+        String updatedFirstName = "Elton";
+        String updatedLastName = "John";
+        String updatedPhoneNumber = "222-222-2222";
+        String updatedEmail = "elton.john@yahoo.com";
+
+        if ((updatedFirstName.isEmpty()) || (updatedLastName.isEmpty()
+                || (updatedPhoneNumber.isEmpty()) || (updatedEmail.isEmpty()))) { //Verify if any fields are empty
+            System.out.println("All fields are required!");
+            return;
+        }
+
+        if (!(isAlphabetic(updatedFirstName) && isAlphabetic(updatedLastName))) { //Verify the first and last name are alphabetic
+            String nameErrorMessage = "First and last name must be alphabetic!";
+            JOptionPane.showMessageDialog(null, nameErrorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        if (!isValidEmail(updatedEmail)) { //Verify valid email address
+            String emailErrorMessage = "Invalid email address!";
+            JOptionPane.showMessageDialog(null, emailErrorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        vetDatabase = new Database();
+        if (!vetDatabase.open()) { //Attempt to open a connection with the database
+            System.out.println("Can't connect to the database!");
+            return;
+        }
+
+        int id = vetDatabase.getClientID(currentPhoneNumber);
+
+        if (!vetDatabase.updateClient(id, updatedFirstName, updatedLastName, updatedPhoneNumber, updatedEmail)) {
+            String errorMessage = vetDatabase.getErrorMessage();
+            JOptionPane.showMessageDialog(null, errorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+        } else {
+            System.out.println("Updating client was successful!");
+        }
+        vetDatabase.close();
+
+    } //end of editClient()
 
 } //end of VetClinic
